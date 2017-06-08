a547f1936edceb5732824425ed2df64cefae3d16
KentoFuruhashi <al13095@shibaura-it.ac.jp>
2017-06-08 18:07:46 +0900

    before desirialize

--- client/client.c
+++ client/client.c
@@ -2,60 +2,9 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include "src/parser.h"
 
 #define PORT 4444
-
-void hexDump (char *desc, void *addr, int len) {
-    int i;
-    unsigned char buff[17];
-    unsigned char *pc = (unsigned char*)addr;
-
-    // Output description if given.
-    if (desc != NULL)
-        printf ("%s:\n", desc);
-
-    if (len == 0) {
-        printf("  ZERO LENGTH\n");
-        return;
-    }
-    if (len < 0) {
-        printf("  NEGATIVE LENGTH: %i\n",len);
-        return;
-    }
-
-    // Process every byte in the data.
-    for (i = 0; i < len; i++) {
-        // Multiple of 16 means new line (with line offset).
-
-        if ((i % 16) == 0) {
-            // Just don't print ASCII for the zeroth line.
-            if (i != 0)
-                printf ("  %s\n", buff);
-
-            // Output the offset.
-            printf ("  %04x ", i);
-        }
-
-        // Now the hex code for the specific character.
-        printf (" %02x", pc[i]);
-
-        // And store a printable ASCII character for later.
-        if ((pc[i] < 0x20) || (pc[i] > 0x7e))
-            buff[i % 16] = '.';
-        else
-            buff[i % 16] = pc[i];
-        buff[(i % 16) + 1] = '\0';
-    }
-
-    // Pad out last line if not exactly 16 characters.
-    while ((i % 16) != 0) {
-        printf ("   ");
-        i++;
-    }
-
-    // And print the final ASCII bit.
-    printf ("  %s\n", buff);
-}
 
 int main(int argc, char const *argv[]) {
     struct sockaddr_in addr;
@@ -71,7 +20,7 @@ int main(int argc, char const *argv[]) {
 
     int len;
     unsigned char *buf = malloc(sizeof(unsigned char) * 64);
-    unsigned char *byte_list = malloc(sizeof(unsigned char) * 64 * 64);
+    unsigned char *byte_list = malloc(sizeof(unsigned char) * 64);
     int list_length = 0;
     while((len = recv(sock,buf, sizeof(buf), 0)) != 0) {
         if(len < 0) {
@@ -84,9 +33,8 @@ int main(int argc, char const *argv[]) {
         }
         list_length += len * sizeof(unsigned char);
     }
-    hexDump("byte_list:", byte_list, list_length);
-
-    
+    free(buf);
+    parse(byte_list, list_length);
 
     close(sock);
     return 0;
---
+++ client/src/parser.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "parser.h"
+
+int hexDump (char *desc, void *addr, int len, int progcount) {
+    int i;
+    unsigned char buff[17];
+    unsigned char *pc = (unsigned char*)addr;
+    // Output description if given.
+    if (desc != NULL)
+        printf ("%s:\n", desc);
+    if (len == 0) {
+        printf("  ZERO LENGTH\n");
+        return -1;
+    }
+    if (len < 0) {
+        printf("  NEGATIVE LENGTH: %i\n",len);
+        return -1;
+    }
+    // Process every byte in the data.
+    for (i = 0; i < len; i++) {
+        // Multiple of 16 means new line (with line offset).
+        if ((i % 16) == 0) {
+            // Just don't print ASCII for the zeroth line.
+            if (i != 0)
+                printf ("  %s\n", buff);
+            // Output the offset.
+            printf ("  %04x ", i);
+        }
+        // Now the hex code for the specific character.
+        printf (" %02x", pc[i]);
+        // And store a printable ASCII character for later.
+        if ((pc[i] < 0x20) || (pc[i] > 0x7e))
+            buff[i % 16] = '.';
+        else
+            buff[i % 16] = pc[i];
+        buff[(i % 16) + 1] = '\0';
+    }
+    // Pad out last line if not exactly 16 characters.
+    while ((i % 16) != 0) {
+        printf ("   ");
+        i++;
+    }
+    // And print the final ASCII bit.
+    printf ("  %s\n", buff);
+    return progcount + len;
+}
+
+void parse(unsigned char *byte_list, int length) {
+    int pc = 0;
+    hexDump("hexdump", byte_list, length, 0);
+    pc = hexDump("magic", &byte_list[pc], 2, pc);
+    pc = hexDump("version", &byte_list[pc], 2, pc);
+}
